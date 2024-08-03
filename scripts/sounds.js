85ecdf678542647f4d74f2ccbc918a8f001e04fb
wikihowadventurecruise <richardbarr1991@gmail.com>
2024-08-03 14:58:31 +0200

    no sounds

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -1,14 +1,14 @@
-var swipe1 = document.getElementById('swipe-1');
-var popSound = document.getElementById('pop-sound');
+// var swipe1 = document.getElementById('swipe-1');
+// var popSound = document.getElementById('pop-sound');
 
-carouselVids.forEach((element, index) => {
-	element.addEventListener("mouseover", function () {
-		popSound.play();
-	});
-});
+// carouselVids.forEach((element, index) => {
+// 	element.addEventListener("mouseover", function () {
+// 		popSound.play();
+// 	});
+// });
 
-carouselVids.forEach((element, index) => {
-	element.addEventListener("click", () => {
-		swipe1.play();
-	})
-});
+// carouselVids.forEach((element, index) => {
+// 	element.addEventListener("click", () => {
+// 		swipe1.play();
+// 	})
+// });
